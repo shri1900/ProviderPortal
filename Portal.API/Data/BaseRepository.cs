677ecaa300909919f2d028428e84ca4c70fea157
Thombre Shrikant <shrikant.thombre@tieto.com>
2019-11-16 11:36:56 +0530

    Added BaseRespository interface and class

---
+++ Portal.API/Data/BaseRepository.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Portal.API.Data;
+using Portal.API.Models;
+
+namespace Portal.API.Data
+{
+    public class BaseRepository : IBaseRepository
+    {
+        private readonly DataContext context;
+
+        public BaseRepository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public void Add<T>(T entity) where T : class
+        {
+            context.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            context.Remove(entity);
+        }
+
+        public async Task<User> GetUser(int userId)
+        {
+            var user = await context.Users.Include(c=>c.UserRoles).FirstOrDefaultAsync(u=>u.UserId == userId);
+            return user;
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            var user = await context.Users.Include(c=>c.UserRoles).ToListAsync();
+            return user;
+        }
+
+        public async Task<bool> SaveAll()
+        {
+            return await context.SaveChangesAsync() > 0;
+        }
+    }
+}
---
+++ Portal.API/Data/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Portal.API.Models;
+
+namespace Portal.API.Data
+{
+    public interface IBaseRepository
+    {
+         void Add<T>(T entity) where T: class;
+         void Delete<T>(T entity) where T: class;
+         Task<bool> SaveAll();
+         Task<IEnumerable<User>> GetUsers();
+         Task<User> GetUser(int userId);
+    }
+}
